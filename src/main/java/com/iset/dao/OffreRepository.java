e5afcc8f39c7b80f4ca5015808e18052834e25eb
amalromdhane <romdhane.996@gmail.com>
2024-10-15 23:04:39 +0200

    first commit

---
+++ src/main/java/com/iset/dao/OffreRepository.java
@@ -0,0 +1,10 @@
+package com.iset.dao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.iset.entities.Offre; 
+
+
+
+public interface OffreRepository extends JpaRepository<Offre, Long>{
+
+}
